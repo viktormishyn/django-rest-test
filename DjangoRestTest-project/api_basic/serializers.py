ba882e633f70526754f423785eeacbd744f7a8ad
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-23 21:04:34 +0300

    Modify api_basic.views.ArticleSerializer (fields = '__all__')

--- DjangoRestTest-project/api_basic/serializers.py
+++ DjangoRestTest-project/api_basic/serializers.py
@@ -5,4 +5,5 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id', 'title', 'author']
+        # fields = ['id', 'title', 'author']
+        fields = '__all__'  # gives all fields
