3202df7ecb2648ffd311fd2260ec8fc01deb4c0e
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-23 19:42:53 +0300

    Create 'api/articles/<int:pk>' endpoint ('GET', 'PUT', 'DELETE')

--- DjangoRestTest-project/api_basic/urls.py
+++ DjangoRestTest-project/api_basic/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import article_list
+from .views import article_list, article_detail 
 
 urlpatterns = [
     path('api/articles', article_list),
+    path('api/articles/<int:pk>', article_detail),
 ]
