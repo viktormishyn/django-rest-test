212d08ce02813d4a219212d8a2f5258112d90538
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-24 00:08:24 +0300

    Create api_basic.views.ArticleViewSet

--- DjangoRestTest-project/api_basic/urls.py
+++ DjangoRestTest-project/api_basic/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 # from .views import article_list, article_detail
 # from .views import ArticleAPIView, ArticleDetailAPIView
-from .views import GenericArticleAPIView, GenericArticleDetailAPIView
+from .views import GenericArticleAPIView, GenericArticleDetailAPIView, ArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('articles', ArticleViewSet, basename='articles')
 
 urlpatterns = [
     # path('api/articles', article_list),
@@ -10,4 +14,6 @@ urlpatterns = [
     # path('api/articles/<int:id>', ArticleDetailAPIView.as_view()),
     path('api/articles', GenericArticleAPIView.as_view()),
     path('api/articles/<int:id>', GenericArticleDetailAPIView.as_view()),
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>', include(router.urls)),
 ]
--- DjangoRestTest-project/api_basic/views.py
+++ DjangoRestTest-project/api_basic/views.py
@@ -3,8 +3,8 @@ from .models import Article
 from .serializers import ArticleSerializer
 
 # from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# from rest_framework import status
+from rest_framework.response import Response
+from rest_framework import status
 
 # from rest_framework.views import APIView
 # from rest_framework.exceptions import NotFound
@@ -15,11 +15,48 @@ from rest_framework import mixins
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+
+
+class ArticleViewSet(viewsets.ViewSet):
+    def list(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class GenericArticleAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
-    authentication_classes = [TokenAuthentication, SessionAuthentication, BasicAuthentication]
+    authentication_classes = [TokenAuthentication,
+                              SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
@@ -34,7 +71,8 @@ class GenericArticleDetailAPIView(generics.GenericAPIView, mixins.RetrieveModelM
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
     lookup_field = 'id'
-    authentication_classes = [TokenAuthentication, SessionAuthentication, BasicAuthentication]
+    authentication_classes = [TokenAuthentication,
+                              SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request, id):
