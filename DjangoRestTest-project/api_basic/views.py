f569fd3d6fee1ccebffc298d0026764bb26cddf6
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-23 23:27:40 +0300

    Add TokenAuthentication to view classes

--- DjangoRestTest-project/api_basic/views.py
+++ DjangoRestTest-project/api_basic/views.py
@@ -12,14 +12,14 @@ from .serializers import ArticleSerializer
 from rest_framework import generics
 from rest_framework import mixins
 
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 
 class GenericArticleAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [TokenAuthentication, SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
@@ -34,7 +34,7 @@ class GenericArticleDetailAPIView(generics.GenericAPIView, mixins.RetrieveModelM
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
     lookup_field = 'id'
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [TokenAuthentication, SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request, id):
