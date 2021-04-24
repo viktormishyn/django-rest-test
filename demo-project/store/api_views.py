9d516c08adf3e905cc47aa858f88dd0f68399d3d
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-24 16:19:02 +0300

    Create store.api_views.ProductDestroy, add 'api/v1/products/<int:id>/destroy' endpoint

--- demo-project/store/api_views.py
+++ demo-project/store/api_views.py
@@ -1,5 +1,5 @@
 from rest_framework.exceptions import ValidationError
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
 from store.serializers import ProductSerializer
 from store.models import Product
 
@@ -58,4 +58,17 @@ class ProductCreate(CreateAPIView):
         except ValueError:
             raise ValidationError({'price': 'valid number is required'})
         return super().create(request, *args, **kwargs)
-            
+
+
+class ProductDestroy(DestroyAPIView):
+    queryset = Product.objects.all()
+    lookup_field = 'id'
+
+    def delete(self,request, *args, **kwargs):
+        # overwrite delete method in order to clear all caches related to the destroyed object
+        product_id = request.data.get('id')
+        response = super().delete(request, *args, *kwargs)
+        if response.status_code == 204:
+            from django.core.cache import cache
+            cache.delete('product_data_{}'.format(product_id))
+        return response
